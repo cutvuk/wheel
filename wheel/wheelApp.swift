0f26d568c009e1ceb89c056c995d0fcff52c4e1a
Aleksandr Khrebtov <planoed@live.ru>
2024-08-29 21:18:37 +0300

    Initial Commit

---
+++ wheel/wheelApp.swift
@@ -0,0 +1,17 @@
+//
+//  wheelApp.swift
+//  wheel
+//
+//  Created by Aleksandr Khrebtov on 29.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct wheelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
